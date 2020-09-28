1481e29a95c6334464e3019930a2c20fa72094ef
CindyTham <cindytham@hotmail.com>
2020-09-28 15:36:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+print('Hello, Lets play the joke game, firstly let me get your name?')
+name = input('')
+print('OK ' + name + ' what is your favorate number between 1 and 9? I have a joke behind each number.')
+number = input ('')
+
+if number == '1':
+    print ('what is the best thing about Switzerland? I dont know but the flag is a big plus sign')
+
+if number == '2':
+    print ('How do you make a tissue dance? Put a little boogie in it')
+
+if number == '3':
+    print ('Why did the farmer win an award? He was outstanding in his field.')
+
+if number == '4':
+    print ('What is red and smells like blue paint? Red paint.')
+
+if number == '5':
+    print ('What is red and smells like blue paint? Red paint.')
+
+if number == '6':
+    print ('Why aren’t koalas actual bears? They don’t meet the koalafications.')
+
+if number == '7':
+    print ('I told my friend 10 jokes to get him to laugh. Sadly, no pun in 10 did.')
+
+if number == '8':
+    print ('knock knock. Who’s There? To. To Who? It’s To Whom.')
+
+if number == '9':
+    print ('How does NASA organize a party? They planet.')
+
+print('I bet that made you laugh')
